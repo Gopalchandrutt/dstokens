1e95e9a1587cfa3d94e5a89cbced3b6b51509b57
Thirumoorthy <thirumoorthy.ponnusamy@trustrace.com>
2023-04-10 14:58:09 +0530

    token config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,15 @@
+const { filterTokensByType } = require("./utils/fns");
+const tokens = require("./src/assets/style/global.json");
+
+const colors = filterTokensByType("color", tokens);
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 };
