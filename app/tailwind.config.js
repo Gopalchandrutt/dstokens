993813de051dfe5b5b5698aa5294356a21c9539c
Thirumoorthy <thirumoorthy.ponnusamy@trustrace.com>
2023-04-11 22:20:04 +0530

    tw config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,7 +4,8 @@ const tokens = require("./src/assets/style/global.json");
 const colors = filterTokensByType("color", tokens);
 const btnSize = flatIt(filterTokensByType("sizing", tokens));
 /** @type {import('tailwindcss').Config} */
-
+console.log(colors);
+console.log(btnSize);
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
