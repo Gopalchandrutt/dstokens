20e3f6189a259a2c1f0dd83052a3084022ae3edc
Thirumoorthy <thirumoorthy.ponnusamy@trustrace.com>
2023-04-12 15:56:09 +0530

    btn config update

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,16 +2,20 @@ const { filterTokensByType, flatIt } = require("./utils/fns");
 const tokens = require("./src/assets/style/global.json");
 
 const colors = filterTokensByType("color", tokens);
+// const buttonColor = filterTokensByType("color", tokens);
 const btnSize = flatIt(filterTokensByType("sizing", tokens));
+const spacing = flatIt(filterTokensByType("spacing", tokens));
 /** @type {import('tailwindcss').Config} */
 console.log(colors);
 console.log(btnSize);
+console.log(spacing);
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       colors,
       height: btnSize,
+      padding: spacing,
     },
   },
   plugins: [],
